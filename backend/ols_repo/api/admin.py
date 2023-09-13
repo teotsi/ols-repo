ae88b203445ca71dd02fcaa42858763af4574ec8
Thodoris Tsigkanos <thodoris.tsigkanos@sportion.net>
2023-09-13 21:39:50 +0300

    add Synonym and Ontology models

--- backend/ols_repo/api/admin.py
+++ backend/ols_repo/api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Term, Synonym, Ontology
+
+admin.site.register(Term)
+admin.site.register(Synonym)
+admin.site.register(Ontology)
--- backend/ols_repo/api/models.py
+++ backend/ols_repo/api/models.py
@@ -1,10 +1,29 @@
 from django.db import models
-
+from random import randint
 # Create your models here.
+
 
 class Term (models.Model):
     label = models.CharField(max_length=256)
     description = models.TextField()
-    
+    id = models.CharField(
+        max_length=12, default=f'FAKE_{randint(0, 9999999)}', primary_key=True)
+
     def __str__(self):
         return self.label
+
+
+class Synonym (models.Model):
+    label = models.CharField(max_length=256, unique=True)
+    term_id = models.ForeignKey(Term, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.label
+
+
+class Ontology (models.Model):
+    child_term_id = models.ForeignKey(Term, on_delete=models.CASCADE, related_name="child_term")
+    parent_term_id = models.ForeignKey(Term, on_delete=models.CASCADE, related_name="parent_term")
+
+    def __str__(self):
+        return f'{self.parent_term_id} > {self.child_term_id}'
