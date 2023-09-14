e502df9acfcd62a87477bb248566c09ceeab8770
Thodoris Tsigkanos <thodoris.tsigkanos@sportion.net>
2023-09-14 18:10:55 +0300

    add all terms endpoint

--- backend/ols_repo/api/views.py
+++ backend/ols_repo/api/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from django.core.serializers import serialize
+from .models import Term
 # Create your views here.
 
+
 def home(request):
+    return HttpResponse('done')
+
+
+def terms(request):
+    if request.method == "GET":
+        all_terms = Term.objects.values()
+        return JsonResponse({"terms": list(all_terms)})
+    return HttpResponse('done')
+
+
+def term(request):
     return HttpResponse('done')
