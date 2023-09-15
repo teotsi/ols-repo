a72b12124d767ffaa0f01508c8b892e2775769e7
Thodoris Tsigkanos <thodoris.tsigkanos@sportion.net>
2023-09-16 00:21:11 +0300

    add ant.d table add fetcher hook clean up styles

--- frontend/terms-table/src/App.js
+++ frontend/terms-table/src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { Table } from "antd";
+import useTerms from "./hooks/useTerms";
+import "./App.css";
 
 function App() {
+  const [pageData, setPageData, data, fetching] = useTerms();
+
+  const columns = [
+    {
+      title: "Label",
+      dataIndex: "label",
+      key: "label",
+    },
+    {
+      title: "Description",
+      dataIndex: "description",
+      key: "description",
+    },
+    {
+      title: "Synonyms",
+      key: "synonyms",
+      dataIndex: "synonyms",
+      render: (_, { synonyms }) => <>{synonyms.join(", ")}</>,
+    },
+    {
+      title: "Parents",
+      key: "parents",
+      dataIndex: "parents",
+      render: (_, { parents }) => <>{parents.join(", ")}</>,
+    },
+  ];
+
+  const formattedData =
+    data?.map((term, index) => ({
+      ...term,
+      key: index,
+    })) ?? [];
+
+  const handlePageChange = (pagination) => {
+    setPageData(pagination);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="centered">
+        <Table
+          columns={columns}
+          dataSource={formattedData}
+          pagination={{ ...pageData, showSizeChanger: false }}
+          onChange={handlePageChange}
+          loading={fetching}
+        />
+      </div>
     </div>
   );
 }
--- frontend/terms-table/src/hooks/useTerms.js
+++ frontend/terms-table/src/hooks/useTerms.js
@@ -1,7 +1,13 @@
 import { useEffect, useState } from "react";
+import { DOMAIN } from "../config";
 
+/**
+ * Hook that fetches terms by page, and stores pagination data for our table
+ * @returns [pagination data, pagination data setter, table data, fetching flag]
+ */
 const useTerms = () => {
   const [data, setData] = useState([]);
+  const [fetching, setFetching] = useState(false);
   const [pageData, setPageData] = useState({
     current: 1,
     total: 1,
@@ -10,15 +16,17 @@ const useTerms = () => {
   const abortController = new AbortController();
 
   const fetcher = async () => {
+    setFetching(true);
     const response = await fetch(
-      `http://localhost:8000/terms?page=${pageData.current}`,
+      `${DOMAIN}/terms/?page=${pageData.current}`,
       {
         signal: abortController.signal,
       }
     );
     const data = await response.json();
     setData(data.terms);
-    const { current, pages: total, size: pageSize } = data.pagination;
+    setFetching(false);
+    const { current, total, size: pageSize } = data.pagination;
     setPageData({ current, total, pageSize });
   };
 
@@ -29,7 +37,7 @@ const useTerms = () => {
     };
   }, [pageData.current]);
 
-  return [pageData, setPageData, data];
+  return [pageData, setPageData, data, fetching];
 };
 
 export default useTerms;
