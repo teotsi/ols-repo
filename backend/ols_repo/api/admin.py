fe9d63a7a710d03bfecb3cca1dfad7732368edb7
Thodoris Tsigkanos <thodoris.tsigkanos@sportion.net>
2023-09-15 03:27:18 +0300

    add separate model for synonym/term rels

--- backend/ols_repo/api/admin.py
+++ backend/ols_repo/api/admin.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Term, Synonym, Ontology
+from .models import Term, Synonym, SynonymAndTerm, Ontology
 
 admin.site.register(Term)
 admin.site.register(Synonym)
+admin.site.register(SynonymAndTerm)
 admin.site.register(Ontology)
--- backend/ols_repo/api/models.py
+++ backend/ols_repo/api/models.py
@@ -15,15 +15,24 @@ class Term (models.Model):
 
 class Synonym (models.Model):
     label = models.CharField(max_length=256, unique=True)
-    term = models.ForeignKey(Term, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f'{self.label} === {self.term}'
+        return f'{self.label}'
+
+
+class SynonymAndTerm(models.Model):
+    term = models.ForeignKey(Term, on_delete=models.CASCADE)
+    synonym = models.ForeignKey(Synonym, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.synonym.label} === {self.term.label}'
 
 
 class Ontology (models.Model):
-    child_term = models.ForeignKey(Term, on_delete=models.CASCADE, related_name="child_term")
-    parent_term = models.ForeignKey(Term, on_delete=models.CASCADE, related_name="parent_term")
+    child_term = models.ForeignKey(
+        Term, on_delete=models.CASCADE, related_name="child_term")
+    parent_term = models.ForeignKey(
+        Term, on_delete=models.CASCADE, related_name="parent_term")
 
     def __str__(self):
         return f'{self.parent_term} > {self.child_term}'
